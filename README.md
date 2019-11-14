feff26f6106bc83dba168460581cdff74978eb3a
hanshirong <1418264042@qq.com>
2019-11-14 19:36:19 +0800

    配置跨域还有封装基础的axios

--- README.md
+++ README.md
@@ -16,3 +16,23 @@ a system to manage own books
 2. 根据提示填就好啦
 3. cd name
 4. 启动`npm run dev`
+5. `npm i element-ui -S`
+6. 在main.js里面
+`import ElementUI from 'element-ui';`
+`import 'element-ui/lib/theme-chalk/index.css';`
+`Vue.use(ElementUI);`
+7. `npm install vuex`
+8. 在src里面新建store文件夹里面新建index.js
+9. 在main.js里面引入store，并且在vue里面添加store
+10. `npm install axios`
+11. 在src里面新建api文件夹，里面放index.js
+12. 在main.js里面引入Api，并且绑定`Vue.prototype.$api=Api;`
+13. 在config文件夹下的index.js里面配置跨域信息
+ proxyTable: {
+      '/api':{
+        target:  'http://localhost:3000',
+        changeOrigin:true,
+      }
+    },
+    
+
---
+++ fronted/src/api/index.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import router from '../router'
+import store from '../store'
+
+let http = axios.create({
+    baseURL: '',
+    withCredentials:true,//跨域
+    headers:{
+        'Content-Type':'application/json;charset=utf-8'
+    },//请求头
+   
+});
+
+function apiAxios(method,url,params,response){
+    http({
+        method:method,
+        url:url,
+        headers: {
+          Authorization: "Bearer " + store.state.token
+        },
+        data:method === 'POST' || method === 'PUT' ? params : null,
+        params: method === 'GET' || method ==='DELETE'? params :null,
+    }).then(function(res){
+        response(res);
+    }).catch(function(err){
+      if (err.response && err.response.status === 401) {
+        router.push({ path: "/login" });
+      } else {
+        response(err);
+      }
+    });
+}
+export default {
+    get: function (url, params, response) {
+      return apiAxios('GET', url, params, response)
+    },
+    post: function (url, params, response) {
+      return apiAxios('POST', url, params, response)
+    },
+    put: function (url, params, response) {
+      return apiAxios('PUT', url, params, response)
+    },
+    delete: function (url, params, response) {
+      return apiAxios('DELETE', url, params, response)
+    }
+}
